9329e29b0f7ccd6159bcca8af6af3800a1feb1c0
LeoObinna <leoaniude20@gmail.com>
2024-05-01 18:10:52 -0700

    API status

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,15 +2,13 @@
 """
 app for registering blueprint and starting flask
 """
+from os import getenv
 from flask import Flask, make_response, jsonify
-from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-from os import getenv
 
 
 app = Flask(__name__)
-CORS(app, origins="0.0.0.0")
 app.register_blueprint(app_views)
 
 
@@ -31,5 +29,6 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
-            port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(getenv('HBNB_API_PORT', '5000'))
+    app.run(host=HOST, port=PORT, threaded=True)
